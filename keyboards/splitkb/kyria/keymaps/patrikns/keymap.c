bfec36251d9cf36542cc2e366dd93603c505f9fc
patrikns <patrik.n.samuelsson@gmail.com>
2022-11-30 22:41:25 +0100

    fix for broken copper pad

--- keyboards/splitkb/kyria/keymaps/patrikns/keymap.c
+++ keyboards/splitkb/kyria/keymaps/patrikns/keymap.c
@@ -107,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT(
      KC_ESC  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSPC,
      KC_TAB , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC,KC_TRNS,     KC_TRNS, KC_TRNS, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, SFT_UP,
-                                 KC_LEFT, KC_RIGHT, KC_DOWN, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_ENT
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_SPC,KC_TRNS,     KC_TRNS, KC_TRNS, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, SFT_UP,
+                                 KC_LEFT, KC_RIGHT, KC_DOWN, KC_SPC, NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_ENT
     ),
 
 /*
