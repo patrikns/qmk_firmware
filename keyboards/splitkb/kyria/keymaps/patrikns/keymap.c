cf70b56d282fda41912e7f37162232b050ea8ce3
patrikns <patrik.n.samuelsson@gmail.com>
2022-12-02 00:08:34 +0100

    keymap changes

--- keyboards/splitkb/kyria/keymaps/patrikns/keymap.c
+++ keyboards/splitkb/kyria/keymaps/patrikns/keymap.c
@@ -107,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT(
      KC_ESC  , KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_BSPC,
      KC_TAB , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_SPC,KC_TRNS,     KC_TRNS, KC_TRNS, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, SFT_UP,
-                                 KC_LEFT, KC_RIGHT, KC_DOWN, KC_SPC, NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_ENT
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , FKEYS,KC_TRNS,     KC_TRNS, KC_TRNS, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, SFT_UP,
+                                 KC_LEFT, KC_RIGHT, KC_DOWN, _______, KC_SPC,     KC_SPC, SYM ,KC_RALT, KC_RGUI, KC_ENT
     ),
 
 /*
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_FUNCTION] = LAYOUT(
       _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     _______, _______, _______, _______, _______, _______,
-      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______,
+      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RSFT, KC_RCTL, KC_LALT, KC_DEL, _______,
       _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
